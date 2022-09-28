189424d7f535bc417f821cc3121dcc64d906273d
VlaDkoR-280 <vladkor280@yandex.ru>
2022-09-28 12:11:29 +0300

    Add method print in class Scanner

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <math.h>
+#include "Scanner.h"
 
 
 //  7 variant
 int main()
 {
+    Scanner scan;
     //double x = -2.235 * pow(10, -2);
     //double y = 2.23;
     //double z = 15.221;
     double x, y, z;
-    scanf_s("%lf%lf%lf", &x, &y, &z);
+    scan.scan(&x, &y, &z);
+    //scanf_s("%lf%lf%lf", &x, &y, &z);
     double f = ((exp(fabs(x - y)) * pow(fabs(x - y), x + y)) / (atan(x) + atan(z))) + (pow(pow(x, 6) + pow(log(y), 2), 1.0/3));
-    printf("%.3f", f);
+    scan.print(f);
 
 }
 //  -0.02235 2.23 15.221
