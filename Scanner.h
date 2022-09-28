1b95e23e78e60c94a5f3d7f4ace71a114e272adb
VlaDkoR-280 <vladkor280@yandex.ru>
2022-09-28 11:47:22 +0300

    Add Class Scanner

---
+++ Scanner.h
@@ -0,0 +1,22 @@
+//
+// Created by vladk on 28.09.2022.
+//
+
+#ifndef UNTITLED_SCANNER_H
+#define UNTITLED_SCANNER_H
+#include "stdio.h"
+#include "math.h"
+
+
+class Scanner {
+public:
+    void scan(double* a, double* b, double* c){
+        //*a = -2.235 * pow(10, -2);
+        //*b = 2.23;
+        //*c = 15.221;
+        scanf_s("%lf%lf%lf", a, b, c);
+    }
+};
+
+
+#endif //UNTITLED_SCANNER_H
